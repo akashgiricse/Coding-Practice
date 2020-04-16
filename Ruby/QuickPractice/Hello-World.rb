d7cda750bc5aaf23c683282a200118b77f755ddc
akashgiricse <akashgiricse@gmail.com>
2020-04-16 08:57:32 +0530

    Remove old code

--- Ruby/QuickPractice/Hello-World.rb
+++
@@ -1,8 +0,0 @@
-require 'sinatra'
-
-get '/' do
-  DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
-              "Thursday", "Friday", "Saturday"]
-  dayname = DAYNAMES[Time.now.wday]
-  "Hello, world! Happy #{dayname}."
-end
